83b486af81d7004af9131edc0f0fb81e97b09c56
alpryakh <alpryakh@gmail.com>
2022-10-27 14:12:44 +0300

    ex19

---
+++ ex 19/Program.cs
@@ -0,0 +1,17 @@
+﻿int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n3 = Convert.ToInt32(Console.ReadLine());
+int n4 = Convert.ToInt32(Console.ReadLine());
+int n5 = Convert.ToInt32(Console.ReadLine());
+
+
+int[] num = {n1, n2, n3, n4, n5};
+
+if (num[0] == num[4] | num[1]==num[3])
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
